bcdf5f95c9511573699d66e8a922e4d86891c15c
Daniel Noe <dnoe@retina.stanford.edu>
2014-01-18 16:03:31 -0800

    new rails app

---
+++ spork/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spork::Application.config.secret_key_base = '46e93467192e17b4f0702c285fa0ad868704bcbb0284b400ab8ff91475db16f7026b893fc9d44f42ff31c3798b6bbddbc4aa649f1fb3050f4d0b22199270a2dd'

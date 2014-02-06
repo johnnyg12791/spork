69f6b38ee2541b37b79ae427ae9850940d927cd1
Maria Isabel Vega <mivega@dn51ucno.sunet>
2014-02-06 01:56:31 -0800

    created search results controller and view

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,10 @@ class Food < ActiveRecord::Base
 	has_many	:ratings
 	has_and_belongs_to_many	:tags
 
-    validates :name, :length => { :minimum => 1 }
+    validates :dish_name, :length => { :minimum => 1 }
+
+    def self.filter(query)
+    	return ["optiooooonsssss", "more options!"]
+    end
 
 end
--- app/models/ratings.rb
+++ app/models/ratings.rb
@@ -3,8 +3,8 @@ class Ratings < ActiveRecord::Base
 	belongs_to	:drink
 	belongs_to	:user
 
-	validates :score : numericality: {less_than_or_equal_to: 5}
-    validates :score : numericality: {greater_than_or_equal_to: 1}
+	validates :score, :numericality => {less_than_or_equal_to: 5}
+    validates :score, :numericality => {greater_than_or_equal_to: 1}
     validates :comment, :length => { :maximum => 200 }
 
 end

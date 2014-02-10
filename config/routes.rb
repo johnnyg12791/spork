272bfd8a0b573e16829b4bf31771c1d36f7d3399
Daniel Noe <dnoe@stanford.edu>
2014-02-10 00:12:24 -0800

    Homepage view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Spork::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

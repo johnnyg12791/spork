f4e1a87233443dd60d577b691642d34504c24ff5
Daniel Noe <dnoe@stanford.edu>
2014-02-11 00:29:49 -0800

    Login modal

--- app/assets/javascripts/facebook_sdk.js
+++ app/assets/javascripts/facebook_sdk.js
@@ -11,15 +11,11 @@ window.fbAsyncInit = function() {
   // whenever someone who was previously logged out tries to log in again, the correct case below 
   // will be handled. 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
-    console.log(response);
-    console.log(response.authResponse.userID);
     // Here we specify what we do with the response anytime this event occurs. 
     if (response.status === 'connected') {
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
-      console.log("1");
-
       testAPI();
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
@@ -29,44 +25,14 @@ window.fbAsyncInit = function() {
       // (1) JavaScript created popup windows are blocked by most browsers unless they 
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
-
-      console.log("2");
-
-      //FB.login();
-       FB.login(function(response) {
-        console.log(authResponse);
-        if(response.authResponse) {
-          console.log("loggedin1");
-        } else {
-          console.log("canceled1");
-        }
-
-      });
-
-
-
+      FB.login();
     } else {
       // In this case, the person is not logged into Facebook, so we call the login() 
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook. 
       // The same caveats as above apply to the FB.login() call here.
-
-      console.log("3");
-
-      FB.login(function(response) {
-        console.log(authResponse);
-        if(response.authResponse) {
-          console.log("loggedin2");
-        } else {
-          console.log("canceled2");
-        }
-
-      });
-
-
-
-
+      FB.login();
     }
   });
   };
@@ -86,29 +52,5 @@ window.fbAsyncInit = function() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Good to see you, ' + response.name + '.');
-      console.log(response);
-
-      var user_data = {first_name: response.first_name, last_name: response.last_name, userID: response.id};
-      
-    $.ajax({
-      url: "/user/main_page",
-      type: "POST",
-      data: user_data
-    }).done(function(data) {
-      console.log(data); 
-      })
-    .fail(function() {
-      alert("error"); 
-    })
-    .always(function() { 
-      alert("complete"); 
-      $(location).attr('href', '/user/main_page')
-    })
-
     });
-
-    
-
-    console.log("ajax");
-
   }
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,18 +3,4 @@ class UserController < ApplicationController
 
   end
 
-  def main_page
-  	data = params[:first_name]#user_data]
-  	users = User.where("fb_id = ?", params[:userID])
-  	if(users.length == 0)
-  		@user = User.new(:first_name => params[:first_name], :last_name => params[:last_name], :fb_id => params[:userID])
-  		@user.save(:validate => false)
-  	else
-  		@user = users
-  	end
-#  	render :nothing => true
-  	#redirect_to(:controller => 'user', :action => 'mainPage') #+ String(@user.id))
-  end
-
-
 end

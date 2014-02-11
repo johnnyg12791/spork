297ba881b23c9297499c4e7579ddb60aa51f0d24
Jordan Segall <jtsegs@gmail.com>
2014-02-11 00:31:58 -0800

    search results - working on query

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,8 +7,19 @@ class ResultsController < ApplicationController
 
 		@search_loc = params[:searchbar]
 		@item_search = params[:itemsearch]
-
-
+		if @item_search != nil
+			@restaurants = Restaurant.select("id").where("lower(name) like ? OR lower(description) like ?", "%#{@item_search.downcase}%", "%#{@item_search.downcase}%")
+			@dishes = []
+			numRestaurants = restaurants.length
+			if numRestaurants != 0
+				for i in 0..numRestaurants
+					@dishes.concat(Food.where("restaurant_id = ?", @restaurants[i].id))
+				end
+			end
+			@dishes.concat(Food.where("lower(dish_name) like ? OR lower(description) like ?", "%#{@item_search.downcase}%", "%#{@item_search.downcase}%"))
+			@dishes.uniq
+			puts @dishes
+		end
 		# @results = foods + drinks
 	end
 

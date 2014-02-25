3ecdb62d38e17a1d33bde22c134cbadba80dbc36
Maria Isabel <mariaisabel.veg@gmail.com>
2014-02-24 23:24:01 -0800

    fixed comment

--- app/controllers/picture_controller.rb
+++ app/controllers/picture_controller.rb
@@ -8,15 +8,6 @@ class PictureController < ApplicationController
 			@photo = Picture.new
 		end
 	end
-
-    t.integer  "imageable_id"
-    t.string   "imageable_type"
-    t.string   "file_name"
-    t.string   "picture_type"
-    t.datetime "date_time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-
 	def create
 		if session["user_id"].nil?
 			render(:action => :new)

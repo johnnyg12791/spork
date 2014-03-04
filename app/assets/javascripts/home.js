9713a6eb3d5f76c3ce52e034e7451c5992640739
Daniel Noe <dnoe@stanford.edu>
2014-03-04 00:55:15 -0800

    Actually readding code that was lost

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,5 @@
 function getCurrLoc() {
+  getCurrLocUI(true);
   var geocoder = new google.maps.Geocoder();
   var pos;
   if(navigator.geolocation) {
@@ -8,12 +9,8 @@ function getCurrLoc() {
         if (status == google.maps.GeocoderStatus.OK) {
           if (results[1]) {
             var searchbar = document.getElementById("search-location");
-            searchbar.value = results[1].formatted_address;
-            var latBar = document.getElementById("lat-location");
-            var lngBar = document.getElementById("lng-location");
-            latBar.value = position.coords.latitude;
-            lngBar.value = position.coords.longitude;
-
+            var formattedAddress = results[1].formatted_address;
+            searchbar.value = formattedAddress;
           }
           else {
             console.log('No results found');
@@ -22,6 +19,7 @@ function getCurrLoc() {
         else {
           console.log('Geocoder failed due to: ' + status);
         }
+        getCurrLocUI(false, formattedAddress);
       });
     });
   } 
@@ -39,7 +37,31 @@ function handleNoGeolocation(errorFlag) {
   }
 }
 
-window.onload=function() {
-  getCurrLoc();
-  document.getElementById("location-button").onclick=getCurrLoc;
+function getCurrLocUI(on, location) {
+  if (on == true) {
+    $("#location-button-icon").hide();
+    $("#location-button-loading-gif").show();
+    $("#location-button").attr("title", "Getting current location");
+    $("#location-button").tooltip();
+    $("#popular-items-heading").html("Getting your location...");
+    $("#popular-items-progress-bar").show();
+    $("#popular-items").fadeTo("fast", 0.25);
+  }
+  if (on == false) {
+    $("#location-button-icon").show();
+    $("#location-button-loading-gif").hide();
+    $("#location-button").attr("title", "Get current location");
+    $("#location-button").tooltip();
+    $("#popular-items-heading").html("Currently popular near " + location + ":");
+    $("#popular-items-progress-bar").slideUp();
+    $("#popular-items").fadeTo("slow", 1.0);
+  }
 }
+
+$(document).ready(function() {
+  getCurrLoc();
+
+  $("#location-button").click(function() {
+    getCurrLoc();   
+  });
+});

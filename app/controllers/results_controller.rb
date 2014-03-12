eeb22b6ee96ca82d1b0e3e65d66ff4cec5346fc3
Daniel Noe <dnoe@stanford.edu>
2014-03-12 00:58:40 -0700

    Results and item-square partials

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,9 +4,9 @@ class ResultsController < ApplicationController
 
 	def search
 
-		@render_json = false
-		if (params[:json] == 'true') then
-			@render_json = true
+		@render = 'default'
+		if params[:render] then
+			@render = params[:render]
 		end
 
 		@search_loc = params[:search_loc]
@@ -25,13 +25,13 @@ class ResultsController < ApplicationController
 		end
 
 		@search_item = ""
-		if (params[:search_item]) then
+		if params[:search_item] then
 			@search_item = params[:search_item]
 		end
 		@search_item.downcase
 
 		@search_distance = 5
-		if(params[:search_distance]) then
+		if params[:search_distance] then
 			@search_distance = params[:search_distance]
 		end
 
@@ -40,35 +40,31 @@ class ResultsController < ApplicationController
 			(3959*acos(cos(radians(?))*cos(radians(restaurants.latitude))*cos(radians(restaurants.longitude)-radians(?)) + 
 			sin(radians(?))*sin(radians(restaurants.latitude)))) < ? AND ((lower(restaurants.name) like ? OR 
 			lower(restaurants.description) like ?) OR (lower(foods.dish_name) like ? OR lower(foods.description) like ?)) AND 
-			foods.restaurant_id = restaurants.id", @search_lat, @search_long, @search_lat, @search_distance, "%#{@search_item}%", 
+			foods.restaurant_id = restaurants.id LIMIT 100", @search_lat, @search_long, @search_lat, @search_distance, "%#{@search_item}%", 
 			"%#{@search_item}%", "%#{@search_item}%", "%#{@search_item}%"])
 
-		@dishes = Food.find_by_sql(["SELECT distinct foods.id, foods.dish_name, foods.price, foods.description, foods.size, 
-			foods.calories, foods.nutrition, foods.presentation from foods, restaurants WHERE 
+		@dishes = Food.find_by_sql(["SELECT distinct foods.id, foods.restaurant_id, foods.dish_name, foods.price, foods.description, 
+			foods.size, foods.calories, foods.nutrition, foods.presentation from foods, restaurants WHERE 
 			(3959*acos(cos(radians(?))*cos(radians(restaurants.latitude))*cos(radians(restaurants.longitude)-radians(?)) + 
 			sin(radians(?))*sin(radians(restaurants.latitude)))) < ? AND ((lower(restaurants.name) like ? OR 
 			lower(restaurants.description) like ?) OR (lower(foods.dish_name) like ? OR lower(foods.description) like ?)) AND 
-			foods.restaurant_id = restaurants.id", @search_lat, @search_long, @search_lat, @search_distance, "%#{@search_item}%", 
+			foods.restaurant_id = restaurants.id LIMIT 100", @search_lat, @search_long, @search_lat, @search_distance, "%#{@search_item}%", 
 			"%#{@search_item}%", "%#{@search_item}%", "%#{@search_item}%"])
 
 		@dishes = @dishes.sort_by do |dish| 
-			if ((Rating.exists? :ratable_id => dish.id) == nil)
+			if Rating.exists? :ratable_id => dish.id == nil
 				0
 			else 
-				Rating.average(:score, :conditions => {:ratable_id => dish.id}) 
+				Rating.average :score, :conditions => {:ratable_id => dish.id}
 			end
 		end
-		@restaurants = @restaurants.map do |rest| {:restaurant => rest, :pictures => rest.pictures} end
-		@dishes = @dishes.map do |dish| {:dish => dish, :pictures => dish.pictures} end	
 
-		if(@render_json) then
-			render(:json => {:restaurants => @restaurants, :dishes => @dishes})
+		if @render == 'json' then
+			render :json => {:restaurants => @restaurants, :dishes => @dishes}
+		elsif @render == 'partials_only' then
+			render :partial => "shared/search_results", :locals => {:restaurants => @restaurants, :dishes => @dishes}
 		end
 
 	end
 
 end
-
-
-
-
